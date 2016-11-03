8c0496c8c5ed9459e7e290c575cde38646f3e699
Krisofer Brink <kpgbrink@gmail.com>
2016-11-02 20:23:43 -0400

    lab 9 almost done!

---
+++ lab09/app/scripts/CommentList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Comment from './Comment.js';
+
+
+export default React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
