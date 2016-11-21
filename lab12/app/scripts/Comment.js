2c44fed1e939ed17e14f3e865a059c3c5907618b
Kristofer Brink <kpb23@calvin.edu>
2016-11-21 13:22:41 -0500

    lab 12 in progress

---
+++ lab12/app/scripts/Comment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Remarkable from 'remarkable';
+
+export default React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = new Remarkable().render(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
