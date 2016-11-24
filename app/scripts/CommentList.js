20de6aeaaecd9f5e7a2bdd964c5bd3285dac97aa
Krisofer Brink <kpgbrink@gmail.com>
2016-11-24 04:22:31 -0500

    Finished lab 12

--- app/scripts/CommentList.js
+++ app/scripts/CommentList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 import Comment from './Comment.js';
 
 
@@ -6,7 +7,7 @@ export default React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function(comment) {
       return (
-        <Comment author={comment.author} key={comment.id}>
+        <Comment id={comment.id} author={comment.author} key={comment.id}>
           {comment.text}
         </Comment>
       );
